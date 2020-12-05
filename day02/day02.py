9bcf3468b404a02eca20146d3ba1b1d8bbcb3f1a
Liz Phillips <lizkristinaphillips@gmail.com>
2020-12-05 12:46:55 -0500

    remove print statements

--- day02/day02.py
+++ day02/day02.py
@@ -2,12 +2,11 @@ def validatePasswords(passworddata):
     validpasswordcount = 0
     for line in passworddata.splitlines():
         policy, policyletter, password = line.split(" ")
-        print(policy, policyletter, password)
         
         index1, index2 = policy.split('-')
         letter1 = password[int(index1)-1]
         letter2 = password[int(index2)-1]
-        print(letter1, letter2, policyletter)
+
         if letter1 == policyletter[0] and letter2 == policyletter[0]:
             continue
         if letter1 == policyletter[0] or letter2 == policyletter[0]:
